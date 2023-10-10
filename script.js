1542b71dac39635db863502623042501519f4cdb
Asma Noor <asma@Asmas-MacBook-Air.local>
2023-10-10 11:50:57 -0500

    Code for 2.2.5

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    console.log("ready")
+
+    $("td:nth-child(3)").each (function(){
+
+        if$((this).text() == "Complete"({
+            $(this).css("background-color", "lightgreen");
+    
+        }
+    });
+    });    
+   
